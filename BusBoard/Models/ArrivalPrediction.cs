095e3b0f4463ba871ef8518ee165ff836b0e9901
Aspinall <paspinall2015@gmail.com>
2023-10-19 14:23:10 +0100

    First 5 buses for chosen stop

--- BusBoard/Models/ArrivalPrediction.cs
+++ BusBoard/Models/ArrivalPrediction.cs
@@ -3,4 +3,6 @@ namespace BusBoard.Models;
 public class ArrivalPrediction
 {
     public int TimeToStation { get; set; }
+    public int LineID { get; set; }
+    public string? DestinationName { get; set; }
 }
--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -7,17 +7,32 @@ internal class Program
 {
     async static Task BusBoard()
     {
+        Console.Write("Please select a stop code: ");
+        string choice = Console.ReadLine()!;
+
         var client = new HttpClient();
         client.BaseAddress = new Uri("https://api.tfl.gov.uk/StopPoint/");
-        var json = await client.GetStringAsync("490000129R/Arrivals");
+        // var json = await client.GetStringAsync("490000129R/Arrivals");
+        var json = await client.GetStringAsync($"{choice}/Arrivals");
         var predictions = JsonConvert.DeserializeObject<List<ArrivalPrediction>>(json);
 
         if (predictions is null)
         {
             throw new Exception("Unable to retrieve predictions from JSON response");
         }
-
-        Console.WriteLine($"Bus arriving in {predictions[0].TimeToStation} seconds");
+        var order = new Dictionary<int, int>();
+        for (int i = 0; i < predictions.Count; i++)
+        {
+            order.Add(i, predictions[i].TimeToStation);
+        }
+        var value = from element in order
+        orderby element.Value ascending
+        select element;
+        Console.WriteLine("The next 5 buses to arrive are: ");
+        foreach (KeyValuePair<int, int> element in value.Take(5))
+        {
+            Console.WriteLine($"The {predictions[element.Key].LineID} bus to {predictions[element.Key].DestinationName}, arriving in {predictions[element.Key].TimeToStation / 60} minutes");
+        }
     }
     
     async static Task Main(string[] args)
